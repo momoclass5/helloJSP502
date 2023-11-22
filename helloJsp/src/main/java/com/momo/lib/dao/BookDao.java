8c13bbf8456821f0f6f1f72e0832a3bc6df2e943
maimari8988 <maimari@empas.com>
2023-11-22 16:35:21 +0900

    도서관리시스템 만들기

---
+++ helloJsp/src/main/java/com/momo/lib/dao/BookDao.java
@@ -0,0 +1,36 @@
+package com.momo.lib.dao;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.momo.common.DBConnPool;
+import com.momo.lib.dto.BookDto;
+
+public class BookDao extends DBConnPool{
+	
+	/**
+	 * 도서목록을 조회 후 리스트를 반환 합니다.
+	 * @return
+	 */
+	public List<BookDto> getList(){
+		String sql = "select * from book";
+		List<BookDto> list = new ArrayList<>();
+		try {
+			stmt = con.prepareStatement(sql);
+			rs = stmt.executeQuery(sql);
+			while(rs.next()) {
+				String no = rs.getString("no");
+				String title = rs.getString("title");
+				String author = rs.getString("author");
+				
+				BookDto dto = new BookDto(no, title, author);
+				list.add(dto);
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return list;
+	}
+}
